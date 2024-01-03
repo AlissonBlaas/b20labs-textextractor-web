96973325a2a6df7bec37d68ff4e29e2bd98d8c05
Alisson Blaas <alissoblaas@gmail.com>
2024-01-03 12:14:54 +0900

    fixing typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex justify-center md:pt-14">
       <div className="flex items-center flex-col md:w-[500px] p-4">
-        <h1>PDF Text Extraction</h1>
+        <strong>PDF Text Extractor</strong>
         <PdfExtractor />
       </div>
     </main>
